84b29abcb6a908a8a71066840243c2ac29395e7d
macbookpro-jerry <jerrychangcn@outlook.com>
2018-07-27 20:40:43 +0800

    引入vue-material

--- web/src/main.js
+++ web/src/main.js
@@ -6,6 +6,11 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(VueMaterial)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
