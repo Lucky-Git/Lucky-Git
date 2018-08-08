2635080f7860d1614a9f6a89ece990449cfb082d
macbookpro-jerry <jerrychangcn@outlook.com>
2018-08-08 17:55:21 +0800

    完善了footer

--- FashionClub/fashionclub/src/main.js
+++ FashionClub/fashionclub/src/main.js
@@ -22,7 +22,12 @@ Vue.use(ElementUI);
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 
-Vue.use(VueMaterial)
+Vue.use(VueMaterial);
+
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+
+Vue.use(VueAwesomeSwiper, /* { default global options } */);
 
 new Vue({
     router,
